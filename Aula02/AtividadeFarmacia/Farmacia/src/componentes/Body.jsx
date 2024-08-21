d4f71fd7410e939df31dd68eabd4254b3e8313fc
EDUARDO BARDINI VITORETI DOS SANTOS <eduardo_bv_santos@estudante.sesisenai.org.br>
2024-08-20 22:15:47 -0300

    Terminei

--- Aula02/AtividadeFarmacia/Farmacia/src/componentes/Body.jsx
+++ Aula02/AtividadeFarmacia/Farmacia/src/componentes/Body.jsx
@@ -1,22 +1,32 @@
 import './Body.css'
 
 function Body(){
+
+  function realizarAgendamento(){
+    alert("Agendamento realizado!");
+  }
+
   return(
    <div className='app-body'>
-     <div className='msgBody'>
+      <div className='msgBody'>
         <h1>Bem vindo, ao Farmárcia</h1>
-        <p>Agora renovada e pronta para lhe atender 24 horas por dia com respeito e dignidade, com os melhores preços da região</p>
+        <p>Estamos muito felizes em anunciar a inauguração da nossa nova farmácia! Na farmárcia, nosso compromisso é proporcionar a você e sua família saúde e bem-estar com excelência e carinho.</p>
         <div className='div-button'>
          <button className='buttons-msgBody' type='button'>Logar-se</button>
          <button className='buttons-msgBody' type='button'>Buscar atendimento</button>
         </div>
-        
-     </div>
+      </div>
 
-
-
-
-   </div>
+      <div className='agendamentoConsultaDiv'>
+        <div className='agendamentoConsultaTable'>
+          <h1>Agendamento</h1>
+          <input className='inputs-agendamentoTable' type="text" placeholder='NOME' />
+          <input className='inputs-agendamentoTable' type="text" placeholder='TELEFONE'/>
+          <input className='inputs-agendamentoTable' type='text' placeholder='DATA'/>
+          <button onClick={realizarAgendamento} className='button-agendamento'>Agendar</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
